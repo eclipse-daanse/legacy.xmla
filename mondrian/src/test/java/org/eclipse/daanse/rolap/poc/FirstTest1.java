c7cdf930f475493b5c41d8edd337888e75f9905f
dbulahov <bulahovdenis@gmail.com>
2026-08-25 18:12:51 +0300

    fix license header Signed-off-by: dbulahov <bulahovdenis@gmail.com>

--- mondrian/src/test/java/org/eclipse/daanse/rolap/poc/FirstTest1.java
+++ mondrian/src/test/java/org/eclipse/daanse/rolap/poc/FirstTest1.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   SmartCity Jena - initial
+ *
+ */
 package org.eclipse.daanse.rolap.poc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,7 +34,7 @@ import org.junit.jupiter.api.Test;
 @RolapContextTest(SchoolTestInstance.class)
 public class FirstTest1 {
 
-    
+
     @Test
     void capturesMemberChildrenSqlFromSchoolCatalog(Connection conn) throws Exception {
         String mdx =
@@ -39,7 +52,7 @@ public class FirstTest1 {
         // The captured SQL is the golden baseline for the future builder-based member mapper.
         captured.forEach(sql -> System.out.println("[captured member SQL] " + sql));
     }
-    
+
     @Test
     @RolapContextTest(SchoolTestInstance.class)
     void capturesMemberChildrenSqlFromSchoolCatalogWithAnnotation(Connection conn) throws Exception {
@@ -78,7 +91,7 @@ public class FirstTest1 {
                 "expected at least one member SQL statement to be captured via RolapUtil hook");
         // The captured SQL is the golden baseline for the future builder-based member mapper.
         captured.forEach(sql -> System.out.println("[captured member SQL] " + sql));
-        
+
         captured = new CopyOnWriteArrayList<>();
         RolapUtil.setHook(conn.getContext(), captured::add);
         try {
@@ -98,7 +111,7 @@ public class FirstTest1 {
                 "SELECT {[Measures].[Anzahl Schulen]} ON COLUMNS FROM [Schulen in Jena (Institutionen)]";
         //conn.getCacheControl(null).flushSchemaCache();
         List<String> captured = new CopyOnWriteArrayList<>();
-        
+
         RolapUtil.setHook(conn.getContext(), captured::add);
         try {
             assertNotNull(executeQuery(conn, mdx));
@@ -126,9 +139,9 @@ public class FirstTest1 {
         assertEquals("[Yearly Income].[Yearly Income].[$90K - $110K]", ResultProvider.getAxis(result, 0, 0, 3));
         assertEquals("[Gender].[Gender].[F]", ResultProvider.getAxis(result, 0, 0, 4));
         assertEquals("[Marital Status].[Marital Status].[M]", ResultProvider.getAxis(result, 0, 0, 5));
-        
+
         mdx = """
-            select 
+            select
                 {[Product].[Food]} on columns,
                 {[Measures].[Unit Sales]} on rows
                 from [Sales]
@@ -139,7 +152,7 @@ public class FirstTest1 {
         assertEquals("[Measures].[Unit Sales]", ResultProvider.getAxis(result, 1, 0, 0));
         assertEquals("191,940", ResultProvider.getCell(result, 0, 0));
 
-        
+
         mdx = """
             SELECT [Measures].[Fact Count] ON COLUMNS,
                 TOPCOUNT([Store Type].[All Store Types].Children, 3, [Measures].[Fact Count]) ON ROWS
@@ -148,11 +161,11 @@ public class FirstTest1 {
         result = executeQuery(conn, mdx);
         assertNotNull(result);
         assertEquals("[Measures].[Fact Count]", ResultProvider.getAxis(result, 0, 0, 0 ));
-        
+
         assertEquals("[Store Type].[Store Type].[Supermarket]", ResultProvider.getAxis(result, 1, 0, 0));
         assertEquals("[Store Type].[Store Type].[Deluxe Supermarket]", ResultProvider.getAxis(result, 1, 1, 0));
         assertEquals("[Store Type].[Store Type].[Mid-Size Grocery]", ResultProvider.getAxis(result, 1, 2, 0));
-        
+
         assertEquals("8", ResultProvider.getCell(result, 0, 0));
         assertEquals("6", ResultProvider.getCell(result, 0, 1));
         assertEquals("4", ResultProvider.getCell(result, 0, 2));
